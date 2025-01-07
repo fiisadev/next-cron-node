e90f3ff7f8fb335f854d3ae19b73ea9e946dfd69
Filip Fišer <fiisadev@gmail.com>
2025-01-07 12:39:58 +0100

    refactor: standalone constructEvent export

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from "./api-keys/types.d";
 export * from "./cron/types.d";
 export * from "./webhooks/types.d";
 export * from "./webhooks/signature-error";
+export { constructEvent } from "./webhooks/webhooks";
--- src/webhooks/webhooks.ts
+++ src/webhooks/webhooks.ts
@@ -3,6 +3,37 @@ import { createHmac, timingSafeEqual } from "crypto";
 import { SignatureError } from "./signature-error";
 import { WebhookEvent } from "./types";
 
+export const constructEvent = (
+  body: string,
+  signature: string,
+  secret: string
+): WebhookEvent => {
+  const expectedSignature = createHmac("sha256", secret)
+    .update(body)
+    .digest("hex");
+
+  let isMatch = false;
+
+  try {
+    isMatch = timingSafeEqual(
+      Buffer.from(expectedSignature),
+      Buffer.from(signature)
+    );
+  } catch (e) {
+    if (e instanceof TypeError) {
+      throw new SignatureError("Invalid signature format");
+    }
+
+    throw e;
+  }
+
+  if (!isMatch) {
+    throw new SignatureError("Signatures do not match");
+  }
+
+  return JSON.parse(body);
+};
+
 export class Webhooks {
   readonly #client: NextCron;
 
@@ -10,34 +41,5 @@ export class Webhooks {
     this.#client = client;
   }
 
-  constructEvent(
-    body: string,
-    signature: string,
-    secret: string
-  ): WebhookEvent {
-    const expectedSignature = createHmac("sha256", secret)
-      .update(body)
-      .digest("hex");
-
-    let isMatch = false;
-
-    try {
-      isMatch = timingSafeEqual(
-        Buffer.from(expectedSignature),
-        Buffer.from(signature)
-      );
-    } catch (e) {
-      if (e instanceof TypeError) {
-        throw new SignatureError("Invalid signature format");
-      }
-
-      throw e;
-    }
-
-    if (!isMatch) {
-      throw new SignatureError("Signatures do not match");
-    }
-
-    return JSON.parse(body);
-  }
+  constructEvent = constructEvent;
 }
